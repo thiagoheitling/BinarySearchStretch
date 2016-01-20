1199601a1cb5e8a42eb6b23b7516efa24d8d1545
Thiago Heitling <thiago.heitling@gmail.com>
2016-01-19 23:19:08 -0800

    first commit

---
+++ BinarySearchStretch/main.m
@@ -0,0 +1,64 @@
+//
+//  main.m
+//  BinarySearchStretch
+//
+//  Created by Thiago Heitling on 2016-01-17.
+//  Copyright © 2016 Thiago Heitling. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NSArray *binarySearchStretch(int inputTarget, NSArray *inputSortedArray) {
+    
+    NSArray *sortedArray = inputSortedArray;
+    NSArray *targetIndexAndNumberOfAppearances = [[NSArray alloc]init];
+    NSNumber *numberOfAppearances = [NSNumber numberWithInt:0];
+    
+    int target = inputTarget;
+    int min = 0;
+    int max = (int)[sortedArray count];
+    int midPointIndex;
+    int appearances = 0;
+    
+    while((min + 1) < max) {
+        midPointIndex = (min + max)/2;
+        if (target < [[sortedArray objectAtIndex:midPointIndex]intValue]) {
+            max = midPointIndex;
+        }
+        else {
+            min = midPointIndex;
+        }
+    }
+    
+    NSNumber *result;
+    
+    if (target == [sortedArray[min]intValue]) {
+        int i = min;
+        while (target == [[sortedArray objectAtIndex:i]intValue]) {
+            appearances++;
+            i--;
+        }
+        result = [NSNumber numberWithInt:(min - appearances + 1)];
+        numberOfAppearances = [NSNumber numberWithInt:appearances];
+    }
+    else {
+        result = [NSNumber numberWithInt:0];
+        numberOfAppearances = [NSNumber numberWithInt:-1];
+    }
+ 
+    targetIndexAndNumberOfAppearances = @[result, numberOfAppearances];
+    NSLog(@"{%@, %@}", targetIndexAndNumberOfAppearances[0], targetIndexAndNumberOfAppearances[1]);
+    
+    return targetIndexAndNumberOfAppearances;
+}
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        int target = 10;
+        NSArray *sortedArray = @[@5, @7, @8, @8, @10, @10, @11, @11, @11, @14];
+        binarySearchStretch(target, sortedArray);
+        
+        return 0;
+    }
+}
